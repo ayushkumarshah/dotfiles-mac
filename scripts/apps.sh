9818f9d762a13a5be0fafb2c29afe7e7e12cdef3
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-04-18 15:51:43 +0545

    Update Brewfile and defaults

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -5,3 +5,5 @@
 # unzip awscli-bundle.zip
 # sudo . ~/Downloads/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
